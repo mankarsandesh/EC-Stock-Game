6912252c5edf96ca71fb10388f43d333f8c62e7d
macky <bbmek999@users.noreply.github.com>
2019-10-16 18:15:23 +0700

    multi game

---
+++ pages/modern/multigame/_id.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <betButton :stockName="$route.params.id" :loop="getLoop($route.params.id)"></betButton>
+  </div>
+</template>
+<script>
+import { mapGetters} from 'vuex'
+import betButton from "~/components/modern/betButton";
+import chartApp from "~/components/modern/chart";
+export default {
+  layout: "desktopModern",
+  components: {
+    betButton,
+    chartApp
+  },
+  computed:{
+      ...mapGetters([
+          "getLoop"
+      ])
+  }
+};
+</script>
