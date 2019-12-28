3d87c7c60ced415d99af8e1de77c3656794024a6
sandesh mankar <sandeshtnk11@gmail.com>
2019-12-28 12:57:58 +0800

    Full screen Total Bet

--- pages/modern/fullscreen/_id.vue
+++ pages/modern/fullscreen/_id.vue
@@ -179,15 +179,7 @@ export default {
             trendType: "twoDigit",
             dialogOtherstock: false,
             //winner mqrquee
-            winner: [
-                "<span class='text-white'><i class='fa fa-bell'></i> Player 001201909101330002WIN $10000, WIN SINA 2019-09-10 14:29:36 </span>",
-                "<span class='text-white'><i class='fa fa-bell'></i> Player 001201909101330002WIN $10000, WIN SINA 2019-09-10 14:29:36 </span>",
-                "<span class='text-white'><i class='fa fa-bell'></i> Player 001201909101330002WIN $10000, WIN SINA 2019-09-10 14:29:36 </span>",
-                "<span class='text-white'><i class='fa fa-bell'></i> Player 001201909101330002WIN $10000, WIN SINA 2019-09-10 14:29:36  </span>",
-                "<span class='text-white'><i class='fa fa-bell'></i> Player 001201909101330002WIN $10000, WIN SINA 2019-09-10 14:29:36 </span>",
-                "<span class='text-white'><i class='fa fa-bell'></i> Player 001201909101330002WIN $10000, WIN SINA 2019-09-10 14:29:36 </span>",
-                "<span class='text-white'><i class='fa fa-bell'></i> Player 001201909101330002WIN $10000, WIN SINA 2019-09-10 14:29:36 </span>"
-            ],
+            winner: [],
             pauseTime: 2000,
             pauseOnHover: false,
             scrollSpeed: 30,
@@ -209,6 +201,7 @@ export default {
         this.getSotckId()
     },
     mounted() {
+        this.getwinuser();
         setTimeout(() => {
             this.getliveBetCount()
             this.getliveAll()
@@ -245,6 +238,33 @@ export default {
         ])
     },
     methods: {
+
+    getwinuser() {
+       this.$axios.$get("api/fetchBet").then(response => {
+         console.log("response.....................")
+        console.log(response.data)
+         console.log("response.......................")
+         let resultStatus  = null;
+          for (let i = 0; i < response.data.length - 1; i++) {
+            let betID = response.data[i].betId;
+            let result = response.data[i].result;
+            let name = response.data[i].name;
+            if(result == 0){ resultStatus = "Loss"; console.log("LOSSSSS"); }else{ resultStatus = "Win";}
+             console.log(resultStatus);
+            let betAmount = response.data[i].betAmount;
+            let betTime = response.data[i].betTime;
+            let win = `<span class="text-slide text-white"><span class="text-warning">
+          <i class="fa fa-bell"></i>
+          </span>Player ${betID}, <span class="text-warning">${resultStatus} $${betAmount},
+          </span> ${name}  ${betTime}</span>`
+            this.winner.push(win);
+          }
+        })
+        .catch(error => {
+          alert(error)
+        })
+       },
+
         test() {
             console.warn(this.$router.history);
         },
