0d44472d3447253c28019e968f40b4e684b81fee
sandesh mankar <sandeshtnk11@gmail.com>
2020-02-11 17:56:57 +0800

    all rule digit button style change

--- components/modern/tableTrendMap.vue
+++ components/modern/tableTrendMap.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <v-layout wrap pa-4 row style="padding-bottom:0 !important;padding-top:48px !important">
-      <v-flex xs2 lg2 v-if="!isFullscreen"  > 
+      <v-flex xs2 lg2 v-if="!isFullscreen">
         <v-layout>
-          <v-flex xs9 >
+          <v-flex xs9>
             <v-btn
               block
-              class="text-white"
+              class="text-white firstDigit"
               color="#003e70"
               @click="changeChartType('firstDigit')"
             >{{$t('gamemsg.firstdigit')}}</v-btn>
@@ -18,7 +18,7 @@
           <v-flex xs9>
             <v-btn
               block
-              class="text-white"
+              class="text-white lastDigit"
               color="#003e70"
               @click="changeChartType('lastDigit')"
             >{{$t('gamemsg.lastdigit')}}</v-btn>
@@ -30,7 +30,7 @@
           <v-flex xs9>
             <v-btn
               block
-              class="text-white"
+              class="text-white bothDigit"
               color="#003e70"
               @click="changeChartType('bothDigit')"
             >{{$t('gamemsg.bothdigit')}}</v-btn>
@@ -43,7 +43,7 @@
           <v-flex xs9>
             <v-btn
               block
-              class="text-white"
+              class="text-white twoDigit"
               color="#003e70"
               @click="changeChartType('twoDigit')"
             >{{$t('gamemsg.twodigit')}}</v-btn>
@@ -52,10 +52,10 @@
           <v-flex class="triangle-right" v-show="trendType=='twoDigit'"></v-flex>
         </v-layout>
       </v-flex>
-      <v-flex class="xs10"> 
-        <v-layout row wrap >
-           <v-flex xs12 lg12 md12 ≈>
-            <trendMap 
+      <v-flex class="xs10">
+        <v-layout row wrap>
+          <v-flex xs12 lg12 md12 ≈>
+            <trendMap
               :dataArray="getStockCrawlerData($route.params.id)"
               :trendType="trendType"
               :key="getStockCrawlerData($route.params.id)[0].writetime + trendType"
@@ -108,5 +108,27 @@ export default {
   border-left: 20px solid #003e70;
   border-bottom: 18px solid transparent;
   align-self: center;
+}
+
+.firstDigit { 
+  font-weight: bolder;
+  border-radius: 10px;
+  background-image: linear-gradient(to right, #19b9ff 20%, #3a79ff 51%);
+}
+.lastDigit { 
+  text-align: center;
+  border-radius: 10px;
+  font-weight: bolder;  
+  background-image: linear-gradient(to right, #fcc12c 20%, #fe644a 51%);
+}
+.bothDigit { 
+  font-weight: bolder;
+  border-radius: 10px;
+  background-image: linear-gradient(to right, #be3095 20%, #e41273 51%);
+}
+.twoDigit {  
+  font-weight: bolder;
+  border-radius: 10px;
+  background-image: linear-gradient(to right, #25dac2 20%, #0287db 51%);
 }
 </style>
