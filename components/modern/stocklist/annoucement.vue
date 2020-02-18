29737435b27574e1e780750530884aa52ac56553
sandesh mankar <sandeshtnk11@gmail.com>
2020-02-18 17:32:09 +0800

    announcement design done

--- components/modern/stocklist/annoucement.vue
+++ components/modern/stocklist/annoucement.vue
@@ -1,27 +1,26 @@
 <template>
-  <div>
-    <v-flex xs12>
-      <table class="table">
+ 
+  
+     <table class="table">
         <thead class="thead-dark">
           <tr>
             <th scope="col" class="bg-colors">{{$t('msg.titles')}}</th>
-            <th scope="col" class="bg-colors">{{$t('msg.preview')}}</th>
             <th scope="col" class="bg-colors">{{$t('msg.date')}}</th>
-          </tr>
+            <th scope="col" class="bg-colors">{{$t('msg.preview')}}</th>
+           </tr>
         </thead>
-        <tbody>
-           <tr v-for="(item,index) in getAnnoucement" :key="index">
+        <tbody style="text-align:center;">
+           <tr  v-for="(item,index) in getAnnoucement" :key="index" class="tabletr">
               <td>{{item.title}}</td>
+               <td>{{item.created_at}}</td>
               <td>{{item.messageContent}}</td>
-              <td>{{item.created_at}}</td>
-          </tr>
+             
+          </tr>          
         </tbody>
-      </table>
-    </v-flex>
-    <v-btn class="main-btn back">
-      <v-icon>arrow_back_ios</v-icon>back
-    </v-btn>
-  </div>
+      </table> 
+      
+
+
 </template>
 <script>
 import { mapGetters, mapActions } from "vuex";
@@ -44,7 +43,29 @@ export default {
 </script>
 
 <style scoped>
+table{
+  border:none;
+  /* border:1px solid red; */
+  border-radius: 10px;
+  background-color: #FFF;
+  box-shadow: 5px 10px 18px #888888;
+}
+
+.thead-dark{
+    border-radius:10px !important;
+  color:#FFF;
+  background-color:#f5f5f5;
+}
 .bg-colors{
-  background-color: #003e70 !important;
+  font-size:18px;
+  color: #333;
+  background-color: #f5f5f5 !important;
+  border-bottom: 1px solid #ddd;
+  padding: 10px;
+}
+tbody tr td{
+ 
+  padding:20px;
+  font-size:16px;
 }
 </style>
