f0acb50644950205af8dc65b138cd22646f1ef2c
mekky <mackychinma@gmail.com>
2020-02-28 15:25:10 +0700

    online history api stockv2-105

--- test.js
+++ test.js
@@ -1,21 +1,22 @@
 stocks = [
-  {
-    crawlerUrl: `/api/getCrawlerData?stockId=1&limit=100`,
-    stockId: 7,
-    stockName: "btc",
-    loop: 1,
-    type: "cypto",
-    crawlerData: "",
-    reference: "https://www.hbg.com/zh-cn/exchange/?s=btc_usdt"
-  }
-];
-
-// split data that always changes (real time)
-stocks = [
-  {
-    stockId: 7,
-    livePrice: 9263.25,
-    liveTime: 4,
-    timer: 555,
-  }
+  [
+    {
+      stockId: 7,
+      gameID: "123456789",
+      stockName: "btc",
+      loop: 1,
+      type: "cypto",
+      crawlerData: [],
+      reference: "https://www.hbg.com/zh-cn/exchange/?s=btc_usdt"
+    },
+    {
+      stockId: 6,
+      gameID: "555555555",
+      stockName: "btc",
+      loop: 5,
+      type: "cypto",
+      crawlerData: [],
+      reference: "https://www.hbg.com/zh-cn/exchange/?s=btc5_usdt"
+    }
+  ]
 ];
