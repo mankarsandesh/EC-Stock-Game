bf40eeb2759273dd07d054b302479e2c1acdcf4e
vongkeo <vongkeokeosavanh@gmail.com>
2020-02-28 16:58:56 +0700

    fetch API done, but the code is not clear yet have to comment and check again

---
+++ components/modern/currentBet.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-layout row class="justify-center">
+    <v-flex xs12 md12>
+      <v-data-table
+        :headers="head"
+        :items="desserts"
+       :items-per-page="5"
+        ref="table"
+        :search="search"
+     
+      >
+        <template v-slot:items="item">
+          <td>{{item.item.UUID}}</td>
+          <td>{{item.item.betID}}</td>
+          <td>{{item.item.ruleName}}</td>
+          <td>{{item.item.betAmount}}</td>
+          <td>{{item.item.rollingAmount}}</td>
+          <td>{{item.item.payout}}</td>
+          <td v-if="item.item.betResult == 'win'">
+            <v-chip color="green" text-color="white">{{item.item.betResult}}</v-chip>
+          </td>
+          <td v-if="item.item.betResult == 'lose'">
+            <v-chip color="red" text-color="white">{{item.item.betResult}}</v-chip>
+          </td>
+          <td v-if="item.item.betResult == 'pending'">
+            <v-chip color="grey " text-color="white">{{item.item.betResult}}...</v-chip>
+          </td>
+          <td>{{item.item.createdDate}}</td>
+          <td>{{item.item.createdTime}}</td>
+          <td>{{item.item.gameID}}</td>
+          <td>{{item.item.gamePID}}</td>
+          <td>{{item.item.stockName}}</td>
+          <td>{{item.item.gameStartDate}}</td>
+          <td>{{item.item.gameStartTime}}</td>
+          <td>{{item.item.gameStatus}}</td>
+        </template>
+      </v-data-table>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  props: ["head", "desserts"],
+  data:() => ({
+       search: '',
+  })
+};
+</script>
+
+
+
--- pages/modern/desktop/current-bet.vue
+++ pages/modern/desktop/current-bet.vue
@@ -1,30 +1,78 @@
 <template>
   <div>
-    <breadcrumbs title="Current Bet" linkItem="bet-history"  titlebtn="bet-history" />
+    <breadcrumbs title="Current Bet" linkItem="bet-history" titlebtn="bet-history" />
     <v-container>
-      <v-layout row wrap>
-        <v-flex xs12 md12>
-          <currentbet />
-        </v-flex>
-      </v-layout>
+      <currentBet :head="head" :desserts="desserts" />
     </v-container>
   </div>
 </template>
 <script>
-import currentbet from "~/components/classic/currentbetinfo";
+import currentBet from "~/components/modern/currentBet";
 import breadcrumbs from "~/components/breadcrumbs";
+import { mapState } from "vuex";
 export default {
   layout: "desktopModern",
   components: {
-    currentbet,
+    currentBet,
     breadcrumbs
   },
-  data() {
+ data() {
     return {
-      window: 0,
-      tabs: ["current bet", "history"],
-      active: null
+      head: [
+        { text: "UUID", value: "UUID" },
+        { text: "betID", value: "betID" },
+        { text: "ruleName", value: "ruleName" },
+        { text: "betAmount", value: "betAmount" },
+        { text: "rollingAmount", value: "rollingAmount" },
+        { text: "payout", value: "payout" },
+        { text: "betResult", value: "betResult" },
+        { text: "createdDate", value: "createdDate" },
+        { text: "createdTime", value: "createdTime" },
+        { text: "gameID", value: "gameID" },
+        { text: "gamePID", value: "gamePID" },
+        { text: "stockName", value: "stockName" },
+        { text: "gameStartDate", value: "gameStartDate" },
+        { text: "gameStartTime", value: "gameStartTime" },
+        { text: "gameStatus", value: "gameStatus" },
+       
+        
+      ],
+      desserts: []
     };
+  },
+  computed: {
+    ...mapState(["portalProviderUUID", "headers","userUUID"]) //get 2 data from vuex first, in the computed
+  },
+  mounted() {
+    this.fetch(); // after this component render done, this will call the function from method
+  },
+  methods: {
+    async fetch() {
+      // afer moumted call the functions this method will run the fetch the data from API
+      const data1 = {
+        // before we call the data we should make the object to the send the request with the API
+        portalProviderUUID: this.portalProviderUUID, // get the portal provider uuid from computed that we call from vuex
+        userUUID: this.userUUID, // get the userUUID with the this object
+        version: "0.1", // version of API
+        betResult: [-1], // -1= pending, 0= lose , 1 = win
+        limit: "20", // limit the data we the data come will come only the 20 that we limit in this case
+        offset: "0" // offset or skip the data
+      };
+      const { data } = await this.$axios.post(
+        "http://uattesting.equitycapitalgaming.com/webApi/getAllBets", // after finish crawl the every API will the the baseURL from AXIOS
+        data1, // data object
+        {
+          headers: {
+            Authorization: "Basic VG5rd2ViQXBpOlRlc3QxMjMh" // basic AUTH before send, becase the backend they will check
+          }
+        }
+      );
+      console.log(data)
+      this.desserts = data.data; // after will get the respone the object or array that come with will be equal the array that we create in the data funtion
+    }
   }
 };
 </script>
+
+<style>
+</style>
