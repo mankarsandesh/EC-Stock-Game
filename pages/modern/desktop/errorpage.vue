6c1f48260dd477f26640754892a1f640328e402a
Ritesh TNK <riteshtnk11@gmail.com>
2020-03-05 15:04:58 +0700

    Adding error image display error

---
+++ pages/modern/desktop/errorpage.vue
@@ -0,0 +1,19 @@
+<template>
+<div xs2>
+    <v-container>
+        <v-layout row wrap>
+            <v-flex xs12>
+                <v-img src="\error\404-bg.png" class="mt-3">
+
+                </v-img>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</div>
+</template>
+
+<script>
+export default {
+    layout: "desktopModern",
+};
+</script>
