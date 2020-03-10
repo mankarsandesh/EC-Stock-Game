667886f30387090ddfeb895bc7b05a9d36e5f3e7
Tanveer <tanveertnk11@gmail.com>
2020-03-10 13:03:53 +0700

    Added custom formatter in tooltip property of live bet count chart and fixed api call for fetching current bet

--- components/modern/fullscreenchart.vue
+++ components/modern/fullscreenchart.vue
@@ -17,35 +17,48 @@ export default {
       series: [
         {
           name: "BIG",
-          data: [0,0,0,0]
+          data: [0,0,0,0],
+          betCount: [15, 14, 13, 0]
         },
         {
           name: "SMALL",
-          data: [0,0,4,2]
+          data: [0,0,4,2],
+          betCount: [15, 14, 13, 100]
         },
         {
           name: "ODD",
-          data: [0,0,3,0]
+          data: [0,0,3,0],
+          betCount: [15, 14, 13, 12]
         },
         {
           name: "EVEN",
-          data: [0,0,0,0]
+          data: [0,0,0,0],
+          betCount: [15, 14, 13, 12]
         },
         {
           name: "HIGH",
-          data: [0,0,0,0]
+          data: [0,0,0,0],
+          betCount: [15, 14, 13, 12]
         },
         {
           name: "MID",
-          data: [0,0,0,0]
+          data: [0,0,0,0],
+          betCount: [15, 14, 13, 12]
         },
         {
           name: "LOW",
-          data: [0,0,0,0]
+          data: [0,0,0,0],
+          betCount: [15, 14, 13, 12]
         },
         {
           name: "NUMBER",
-          data: [0,0,0,0]
+          data: [0,0,0,0],
+          betCount: [15, 14, 13, 12]
+        },
+        {
+          name : "TIE",
+          data: [0,0,0,0],
+          betCount: [15, 14, 13, 12]
         }
       ],
       chartOptions: {
@@ -65,16 +78,28 @@ export default {
           width: 1,
           colors: ["#fff"]
         },
-        // title: {
-        //   text: "Live Stock Data"
-        // },
+        title: {
+          text: "Live Bet Data"
+        },
         xaxis: {
           categories: ["First Digit", "Last Digit", "Both Digit", "Two Digit"]
         },
         tooltip: {
+          enabled: true,
+          followCursor: false,
+          intersect: true,
+          onDataSetHover: {
+            highlightDataSeries: false
+          },
           y: {
-            formatter: function(val) {
-              return "$" + val + " users : 20";
+            formatter: function(val, { series, seriesIndex, dataPointIndex, w }) {
+              console.log('ayaaaaaaaaaaaaaaaaaaaaa')
+              return '<div class="arrow_box">' +
+                '<span> Amount $' + series[seriesIndex][dataPointIndex] + ' </span>' +
+                '</div>' +
+                '<div class="arrow_box">' + 
+                '<span> BetCount' + w.config.series[seriesIndex].betCount[dataPointIndex] + '</span>' +
+                '</div>'
             }
           }
         },
