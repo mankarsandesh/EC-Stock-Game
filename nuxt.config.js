351c081bf921be6c0251ee0b230f8d0d1b0dc58d
sandesh mankar <mankarsandesh11@gmail.com>
2020-03-19 10:23:57 +0800

    server setting and pull from version brnach

--- nuxt.config.js
+++ nuxt.config.js
@@ -140,8 +140,8 @@ module.exports = {
     }
   },
   server: {
-    port: 8000, // default: 3000
+    // port: 8000, // default: 3000
 
-    host: "0.0.0.0" // default: localhost
+    // host: "0.0.0.0" // default: localhost
   } // other configs
 };
