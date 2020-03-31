5a7ab3d94a1564c17aa9c93228afe92fe0ad800d
Sandesh Mankar <55863239+mankarsandesh@users.noreply.github.com>
2020-03-31 12:28:50 +0800

    Revert "Stocv2 248 swtich time vong"

--- components/stockSelect.vue
+++ components/stockSelect.vue
@@ -15,7 +15,7 @@
         return-object
       ></v-select>
     </v-flex>
-    <v-flex md3>
+    <v-flex md2>
       <v-select
         v-model="stockName"
         :items="stockNames"
@@ -48,20 +48,26 @@
         return-object
         id="minute"
       >
-        <template slot="selection" slot-scope="data">{{ data.item.loopName }} Minutes</template>
+        <template slot="selection" slot-scope="data"
+          >{{ data.item.loopName }} Minutes</template
+        >
         <template v-slot:item="data">
           <template v-if="typeof data.item !== 'object'">
-            <v-list-tile-content>{{ data.loopName }} Minutes</v-list-tile-content>
+            <v-list-tile-content
+              >{{ data.loopName }} Minutes</v-list-tile-content
+            >
           </template>
           <template v-else>
             <v-list-tile-content>
-              <v-list-tile-title>{{ data.item.loopName }} Minutes</v-list-tile-title>
+              <v-list-tile-title
+                >{{ data.item.loopName }} Minutes</v-list-tile-title
+              >
             </v-list-tile-content>
           </template>
         </template>
       </v-select>
     </v-flex>
-    <v-flex md4>
+    <v-flex md5>
       <v-text-field
         v-model="gameId"
         label="game id"
@@ -71,6 +77,7 @@
         solo
         hide-details
         disabled
+        id="gameId"
       />
     </v-flex>
   </v-layout>
@@ -96,13 +103,12 @@ export default {
     stock(value) {
       let GET_STOCK_TYPE = sessionStorage.getItem("STOCK_TYPE");
       if (value.type === GET_STOCK_TYPE) {
-        this.stockSocket = true;
         // this.stockName = ""; // after value is not empty we clear the value is first
         this.stockNames = [];
         this.stockNames = value.stocks;
         $("#stockName").click();
+        this.stockSocket = true;
       }
-
       // when value is changed this value will do the list
       if (this.stockSocket) {
         // check the stockSocket is come or not
@@ -110,7 +116,6 @@ export default {
       } else {
         // after value is false the logic will be come in this case
         if (value !== "") {
-          sessionStorage.setItem("STOCK_TYPE", value.type);
           // this.stockName = "";
           this.stockNames = value.stocks;
           $("#stockName").click();
@@ -120,49 +125,72 @@ export default {
     },
     stockName(value) {
       let GET_STOCK_TYPE = sessionStorage.getItem("STOCK_TYPE");
-      if (value.stockName !== undefined) {
-        sessionStorage.setItem("STOCK_NAME", this.stockName.stockName);
-      }
       if (this.stock.type === GET_STOCK_TYPE) {
+        console.log("The same stock", value);
         this.minute = "";
         this.minutes = value.loops;
         $("#minute").click();
+      } else {
+        console.log("Is not the same stock", value);
+        if (this.stockSocket) {
+          if (value !== "") {
+            this.minute = "";
+            this.minutes = value.loops;
+            $("#minute").click();
+          }
+        } else {
+          this.stockSocket = false;
+        }
       }
+      // this.stockSocket = true;
     },
     minute(value) {
-      if (value.loopName !== undefined) {
-        sessionStorage.setItem("STOCK_LOOP", value.loopName);
-        sessionStorage.setItem(
-          "STOCK_URL",
-          this.stockName.stockName + value.loopName
-        );
-      }
-      if (value !== "") {
-        this.gameId = "";
-        this.gameId = value.gameID;
+      if (this.stockSocket) {
+        if (value !== "") {
+          this.gameId = "";
+          this.gameId = value.gameID;
+          $("#gameId").click();
+        }
+      } else {
+        this.stockSocket = false;
       }
     },
     gameId(value) {
-      const GET_STOCK_URL = sessionStorage.getItem("STOCK_URL");
-      const GET_STOCK_TYPE = sessionStorage.getItem("STOCK_TYPE");
-      const GET_STOCK_NAME = sessionStorage.getItem("STOCK_NAME");
-      const GET_STOCK_LOOP = sessionStorage.getItem("STOCK_LOOP");
+      if (this.stockSocket) {
+        if (value !== "") {
+          if (this.stock.type == "crypto") {
+            let routeName =
+              this.stockName.stockName + "" + this.minute.loopName;
+            // check is multi game or not
+            if (this.$route.name === "modern-desktop-id") {
+              this.$router.replace(`/modern/desktop/${routeName}`);
+            } else {
+              // if is multi game then add selected game
+              this.addStockMultigame(routeName);
+            }
+          } else {
+            // check is multi game or not
+            if (this.$route.name === "modern-desktop-id") {
+              this.$router.replace(
+                `/modern/desktop/${this.stockName.stockName}`
+              );
+              // if is multi game then add selected game
+            } else {
+              this.addStockMultigame(this.stockName.stockName);
+            }
+          }
 
-      if (GET_STOCK_TYPE == "crypto") {
-        if (this.$route.name === "modern-desktop-id") {
-          this.$router.replace(`/modern/desktop/${GET_STOCK_URL}`);
-        } else {
-          // if is multi game then add selected game
-          this.addStockMultigame(GET_STOCK_URL);
+          // Vong code
+          // if (this.stock.type == "crypto") {
+          //   this.$router.replace(
+          //     `/modern/desktop/${this.stockName.stockName}${this.minute.loopName}`
+          //   );
+          // } else {
+          //   this.$router.replace(`/modern/desktop/${this.stockName.stockName}`);
+          // }
         }
       } else {
-        // check is multi game or not
-        if (this.$route.name === "modern-desktop-id") {
-          this.$router.replace(`/modern/desktop/${GET_STOCK_NAME}`);
-          // if is multi game then add selected game
-        } else {
-          this.addStockMultigame(GET_STOCK_NAME);
-        }
+        this.stockSocket = false;
       }
     },
     getStockCategory(val) {
