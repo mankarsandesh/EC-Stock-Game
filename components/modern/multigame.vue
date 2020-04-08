4086835a66d47c9afb4e19ff3572664652938180
Tanveer <tanveertnk11@gmail.com>
2020-04-08 14:06:02 +0700

    Fixed unknown getter error

--- components/modern/multigame.vue
+++ components/modern/multigame.vue
@@ -147,7 +147,6 @@ export default {
       "getPortalProviderUUID",
       "getTimerByStockName",
       "getStockLoop",
-      "lotterydraw",
       "getStockById",
       "getAmountBettingByStockId"
     ])
