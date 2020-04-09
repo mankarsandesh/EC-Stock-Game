0ff9521f335b7fd764e399be2ff14dc7e540e9d9
Tanveer <tanveertnk11@gmail.com>
2020-04-09 15:19:07 +0700

    Fixed env varibale names

--- config/config.development.js
+++ config/config.development.js
@@ -1,9 +1,9 @@
 const config = {
   version: "1",
-  authUser: `${process.env.AUTHUSER}`,
-  authPassword: `${process.env.AUTHPASS}`,
-  apiDomain: process.env.API_DOMIAN,
-  socketUrl: process.env.SOCKETURL,
+  authUser: `${process.env.AUTH_USER}`,
+  authPassword: `${process.env.AUTH_PASS}`,
+  apiDomain: process.env.API_DOMAIN,
+  socketUrl: process.env.SOCKET_URL,
   baseUrl: process.env.BASE_URL,
   secretKey: "CC21128A312FAF7817C93D1B51CB9",
   header: {
