81415283f81563e4e860811ba70130e2a6d43a5d
sandesh mankar <sandeshtnk11@gmail.com>
2020-04-16 11:19:38 +0800

    Bet Hsitory issue solved

--- pages/modern/desktop/bet-history.vue
+++ pages/modern/desktop/bet-history.vue
@@ -205,7 +205,6 @@ export default {
           userUUID: this.userUUID, // get the userUUID with the this object
           version: config.version, // version of API
           betResult: [0, 1], // -1= pending, 0= lose , 1 = win   
-          offset : 0,         
           dateRangeFrom: this.dateFrom,
           dateRangeTo: this.dateTo
         };
