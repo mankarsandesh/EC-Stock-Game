3de136576d4c0233bb224d7da74472c6e6294f5b
Tanveer <tanveertnk11@gmail.com>
2020-04-27 15:23:04 +0700

    Fixed conflicts

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,20 @@
 <template>
-<v-container fluid grid-list-lg fill-height class="image-bg">
-  <v-fade-transition mode="out-in">
-    <v-layout align-center column>
-      <v-img src="/bg/group33.png" width="500" height="100" />
-      <div class="errorBox" v-if="messageError">
-        <h4 v-for="(data, index) in messageError" :key="index">{{ data }}</h4>
-      </div>
-      <div class="preloader-wrap">
-        <div class="percentage" id="precent"></div>
-        <div class="loader">
-          <div class="trackbar">
-            <div class="loadbar"></div>
+  <v-container fluid grid-list-lg fill-height class="image-bg">
+    <v-fade-transition mode="out-in">
+      <v-layout align-center column>
+        <v-img src="/bg/group33.png" width="500" height="100" />
+        <div class="errorBox" v-if="getLoginError.length > 0">
+          <h4 v-for="(data, index) in getLoginError" :key="index">
+            {{ data }}
+          </h4>
+        </div>
+        <div class="preloader-wrap">
+          <div class="percentage" id="percent"></div>
+          <div class="loader">
+            <div class="trackbar">
+              <div class="loadbar"></div>
+            </div>
+            <div class="glow"></div>
           </div>
           <div class="glow"></div>
         </div>
@@ -36,37 +40,12 @@ export default {
 
   data() {
     return {
-      getUserAuthInfo: "",
-      authUser: "",
-      stockname: "btc1",
-      referrerURL: document.referrer.match(/:\/\/(.[^/]+)/)[1],
-      portalProviderUUID: this.$route.query.portalProviderUUID,
-      portalProviderUserID: this.$route.query.portalProviderUserID,
-      balance: this.$route.query.balance,
-      messageError: [],
-      stockName: "btc1",
+      stockName: config.homePageStockName,
       linkto: ""
     };
   },
   mounted() {
-    if (!this.portalProviderUUID) {
-      const error = "portalProviderUUID field is Missing";
-      this.messageError.push(error);
-    }
-    if (!this.portalProviderUserID) {
-      const error = "portalProviderUserID field is Missing";
-      this.messageError.push(error);
-    }
-    if (!this.balance) {
-      const error = "balance field is Missing";
-      this.messageError.push(error);
-    }
-    if (!this.referrerURL) {
-      const error = "Somthing Wrong.";
-      this.messageError.push(error);
-    }
-    this.setLanguage("cn");
-    this.checkUserAuth();
+    this.getProgress();
   },
   watch: {
     "$screen.width"() {
@@ -78,112 +57,35 @@ export default {
     }
   },
   computed: {
-    ...mapGetters([
-      "getPortalProviderUUID",
-      "getUserUUID",
-      "getUserAuth",
-      "getMessageError"
-    ])
+    ...mapGetters(["getLoginError"])
   },
   methods: {
-    async checkUserAuth() {
-      try {
-        var reqBody = {
-          portalProviderUUID: this.portalProviderUUID,
-          portalProviderUserID: this.portalProviderUserID,
-          version: config.version,
-          ip: "225.457.454.123",
-          domain: this.referrerURL,
-          balance: this.balance
-        };
-        var { data } = await this.$axios.post(
-          config.userLoginAuth.url, // after finish crawl the every API will the the baseURL from AXIOS
-          reqBody, // data object
-          {
-            headers: config.header
-          }
-        );       
-        console.log(data);
-        if (data.status) {
-          const userInfo = {
-            authUser: config.authUser,
-            authPassword: config.authPassword,
-            portalProviderUUID: this.portalProviderUUID,
-            userId: data.data.userUUID,
-            redirect: this.referrerURL
-          };
-          this.setPortalProviderUUID(userInfo.portalProviderUUID);
-          this.setUserUUID(userInfo.userId);
-          localStorage.setItem("USER_UUID", userInfo.userId);
-          localStorage.setItem("PORTAL_PROVIDERUUID", userInfo.portalProviderUUID);
-          // let objJsonStr = JSON.stringify(userInfo);
-          // console.log('stringify user info', objJsonStr)
-          // let buff = new Buffer(objJsonStr);
-          // let base64data = buff.toString("base64");
-          // console.log('base64 data', base64data)
-          if (userInfo.authUser && userInfo.authPassword) {
-            if (userInfo.portalProviderUUID && userInfo.userId) {
-              // let buffDecode = new Buffer(base64data, "base64");
-              // console.log('buffdecode', buffDecode)
-              // let authData = buffDecode.toString("ascii");
-              // console.log('buffauthdata', authData)
-              // this.setAuth(authData);
-              // localStorage.setItem("AUTH", JSON.stringify(base64data));
-              //window.location = this.linkto;
-              this.getProgress();
-              // location.reload(true);
-            } else {
-              const error = "Portal Provider OR userID is Missing...";
-              this.messageError.push(error);
-            }
-          } else {
-            const error = "Authentication authUser & authPassword is Missing.";
-            this.messageError.push(error);
-          }
-        } else {
-          this.messageError.push(config.error.general);
-          throw new Error(config.error.general);
-        }
-      } catch (ex) {
-        console.log(ex);
-        log.error({
-            req: reqBody,
-            res: data,
-            page: "pages/index.vue",
-            apiUrl: config.userLoginAuth.url,
-            provider: localStorage.getItem("PORTAL_PROVIDERUUID"),
-            user: localStorage.getItem("USER_UUID")
-          },
-          ex.message
-        );
-      }
-    },
-    ...mapActions(["setAuth", "setUserUUID","setPortalProviderUUID","setLanguage"]),
-
     getProgress() {
       let width = 100,
         perfData = window.performance.timing, // The PerformanceTiming interface represents timing-related performance information for the given page.
         EstimatedTime = -(perfData.loadEventEnd - perfData.navigationStart),
-        time = parseInt((EstimatedTime / 1000) % 60) * 100;
-      // Loadbar Animation
-      $(".loadbar").animate({
+        time = 1000;
+      // Load bar Animation
+      $(".loadbar").animate(
+        {
           width: width + "%"
         },
         time
       );
-      // Loadbar Glow Animation
-      $(".glow").animate({
+      // Load bar Glow Animation
+      $(".glow").animate(
+        {
           width: width + "%"
         },
         time
       );
 
       // Percentage Increment Animation
-      let PercentageID = $("#precent"),
+      let PercentageID = $("#percent"),
         start = 0,
         end = 100,
-        durataion = time;
-      this.animateValue(PercentageID, start, end, durataion);
+        duration = time;
+      this.animateValue(PercentageID, start, end, duration);
 
       // Fading Out Loadbar on Finised
       setTimeout(function () {
@@ -204,9 +106,9 @@ export default {
         obj.innerHTML = current;
         if (current == end) {
           clearInterval(timer);
-          window.location = isMobile ?
-            "/modern" :
-            "/modern/desktop/" + this.stockName;
+          this.$router.push(
+            isMobile ? "/modern" : `/modern/desktop/${this.stockName}`
+          );
         }
       }, stepTime);
     }
