27eecdb98405a191eb6ce55d0c7b3f4c1ac4ecd9
vongkeo <vongkeokeosavanh@gmail.com>
2020-05-01 14:13:06 +0700

    check in the homepage, multiple and full screen is working the sound when bet also when user win

--- helpers/sound.js
+++ helpers/sound.js
@@ -1,14 +1,22 @@
 class Sound {
+
+    // handle the sound, by name of the sound 
     audio(name) {
         var audio = new Audio(`/voice/${name}`);
         return audio
     }
+
+    // win user win bet use this sound for play 
     winBet() {
-        return this.audio('winbet.mp3').play();
+        return this.audio('winBet.mp3').play();
     }
+
+    // the sound for user hitting the bet
     betTing() {
-        return this.audio('bet-chips.mp3').play();
+        return this.audio('betChips.mp3').play();
     }
 
 }
 export default Sound = new Sound
+
+
