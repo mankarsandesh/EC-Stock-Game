a3ecbc25a41d46073de2ed65acc049e5075b66ef
Tanveer <tanveertnk11@gmail.com>
2020-05-12 15:40:04 +0700

    Fixed translation in bethistory

--- plugins/i18n.js
+++ plugins/i18n.js
@@ -6,7 +6,7 @@ Vue.use(VueI18n);
 
 export default ({ app, store }) => {
   app.i18n = new VueI18n({
-    locale: secureStorage.getItem("lang") || "cn",
+    locale: store.getters.getLocale || "cn",
     fallbackLocale: "cn",
     messages: {
       us: require("~/locales/us.json"),
