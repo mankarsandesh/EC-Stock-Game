48656bb2fb2019446a391af54b9592ad5b0a52af
mekky <mackychinma@gmail.com>
2020-05-14 16:57:25 +0700

    fixed issue profile page

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,11 @@ module.exports = {
       { hid: "description", name: "description", content: pkg.description }
     ],
     link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.png" }
+      { rel: "icon", type: "image/x-icon", href: "/favicon.png" },
+      {
+        rel: "stylesheet",
+        href: "https://use.fontawesome.com/releases/v5.0.13/css/all.css"
+      }
       // {
       //   rel: "stylesheet",
       //   href:
--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -2,6 +2,16 @@ import Vue from "vue";
 import Vuetify from "vuetify/lib";
 import colors from "vuetify/es5/util/colors";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
+Vue.component("font-awesome-icon", FontAwesomeIcon); // Register component globally
+library.add(fas); // Include needed icons.
+
+Vue.use(Vuetify, {
+  iconfont: "faSvg"
+});
 Vue.use(Vuetify, {
   theme: {
     primary: "#121212", // a color that is not in the material colors palette
