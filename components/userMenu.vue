9460e111153b826a2c17e31a301146c94a3bf4ed
Tanveer <tanveertnk11@gmail.com>
2020-05-14 10:35:26 +0700

    Fixed user profile bug

--- components/userMenu.vue
+++ components/userMenu.vue
@@ -91,13 +91,13 @@ export default {
   },
   methods: {
     getUserBanlancePosition() {
-      console.log(document.getElementById("userBanlance").offsetTop);
-      console.log(
-        document.getElementById("userBanlance").offsetParent.offsetParent
-          .offsetLeft
-      );
-      console.log(document.getElementById("betRuleButton").offsetTop);
-      console.log(document.getElementById("betRuleButton").offsetLeft);
+      // console.log(document.getElementById("userBanlance").offsetTop);
+      // console.log(
+      //   document.getElementById("userBanlance").offsetParent.offsetParent
+      //     .offsetLeft
+      // );
+      // console.log(document.getElementById("betRuleButton").offsetTop);
+      // console.log(document.getElementById("betRuleButton").offsetLeft);
     },
     getLogout() {
       this.dialogConfirm = true;
