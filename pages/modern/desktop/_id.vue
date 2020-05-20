358f25f03bd88efb5fbce54eb67b46b3e1e3713b
sandesh <sandeshtnk11@gmail.com>
2020-05-20 15:17:25 +0700

    game Rule issue fixed and basic Changes

--- pages/modern/desktop/_id.vue
+++ pages/modern/desktop/_id.vue
@@ -170,27 +170,24 @@
         <!-- Road Map End -->
       </v-flex>
 
-      <!-- Game Rule Popup -->
-      <v-dialog v-model="dialog" width="800">
+      <!-- Game Rule Popup open First Time -->
+      <v-dialog v-model="GameRuleDialog" width="50%">
         <v-card class="ruleModel" style="border-radius: 10px;">
           <v-icon
             class="closePopup"
             color="#333 !important"
-            @click="dialog = false"
-            >close</v-icon
-          >
-          <v-card-title class="title" primary-title
-            >TOP 10 LEADERS</v-card-title
-          >
-          <v-card-text>
-            <leaderboardUserlist />
+            @click="GameRuleDialog = false"
+            >close</v-icon          >          
+          <v-card-text style="padding:40px;">
+            <h2 style="text-align:center;">EC Gameing Rule </h2>
+             <onlyrules />
           </v-card-text>
           <v-flex class="text-lg-right">
             <v-btn
               class="buttonGreensmall"
-              to="/modern/desktop/leaderboard"
+              to="/modern/desktop/gamerule"
               dark
-              >Go to Leaderboard</v-btn
+              >Gameing Rule</v-btn
             >
           </v-flex>
         </v-card>
@@ -241,12 +238,12 @@ import betButton from "~/components/modern/betButton";
 import chartApp from "~/components/modern/chart";
 import tableTrendMap from "~/components/modern/tableTrendMap";
 import stockSelect from "~/components/stockSelect";
-import leaderboardUserlist from "~/components/modern/leaderboard/leaderboardUserlist";
 import config from "~/config/config.global";
 import lotteryDraw from "~/components/modern/lotteryDraw";
 import { isMobile } from "mobile-device-detect";
 import log from "roarr";
 import secureStorage from "../../../plugins/secure-storage";
+import onlyrules from "~/components/modern/rule/onlyrule";
 
 export default {
   async validate({ params, store }) {
@@ -261,15 +258,15 @@ export default {
     betButton,
     tableTrendMap,
     stockSelect,
-    leaderboardUserlist,
     lotteryDraw,
-    isMobile: isMobile
+    isMobile: isMobile,
+    onlyrules
   },
   data() {
     return {
       routeParams: this.$route.params.id,
       stock: [],
-      dialog: false,
+      GameRuleDialog: true,
       bgColor: "#778899",
       position: "top-right",
       isHidden: false,
@@ -293,12 +290,12 @@ export default {
     }
     this.getStock();
     // Game Rule Popup check and open Ne User
-    // if (secureStorage.getItem("gameRule") != "shown") {
-    //   this.dialog = true;
-    //   secureStorage.setItem("gameRule", "shown");
-    // } else {
-    //   this.dialog = false;
-    // }
+    if (secureStorage.getItem("gameRule") != "shown") {
+      this.GameRuleDialog = true;
+      secureStorage.setItem("gameRule", "shown");
+    } else {
+      this.GameRuleDialog = false;
+    }
   },
   beforeDestroy() {
     this.stopListenSocket(
@@ -488,6 +485,14 @@ export default {
 };
 </script>
 <style scoped>
+.closePopup{
+  background-color: #FFF;
+  right:0;
+  border-radius:50%;
+  padding: 2px;
+  width: 30px;
+  height: 30px;
+}
 .multiGame {
   z-index: 999;
   position: fixed;
